cfe5dca150d12edabda6dcb12441b9b4a790c443
Sinisa vukmirovic <sinisavukmirovic@gmail.com>
2019-08-27 21:01:44 +0200

    Getters & Setters

--- script.js
+++ script.js
@@ -27,3 +27,52 @@ let rectangle1 = new Rectangle(5, 3, 'blue');
 let rectangle2 = new Rectangle(10, 6, 'red');
 
 rectangle1.printDescription();
+
+console.log('======================================');
+
+// 2. Getters & Setters
+// =======================
+
+class Square {
+    constructor( width ) {
+
+        this.width = width;
+        this.height = width;
+        this.numberOfSequestsForArea = 0;
+    }
+
+    // getter
+    get area() {
+        this.numberOfSequestsForArea++;
+        return this.width * this.width;
+    }
+
+    // setter
+    set area(area) {
+        this.width = Math.sqrt(area);
+        this.height = this.width;
+    }
+}
+
+let square1 = new Square(4);
+
+// getter behaves like a property and doesnot have () when called
+console.log(square1.area); 
+
+// setter 
+// a setter assigns new values to instance properties
+square1.area = 36;
+
+console.log(square1.width);
+console.log(square1.height);
+
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.area);
+console.log(square1.numberOfSequestsForArea);
+
+console.log('======================================');
+
+// 3. Static Methods
+// ==================
+
